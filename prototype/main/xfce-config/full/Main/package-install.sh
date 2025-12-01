7ef600eb4b555c1ecb80c68116e859a4dd34b847
samwhelp <samwhelp@users.noreply.github.com>
2025-12-01 16:48:05 +0800

    fix

--- prototype/main/xfce-config/full/Main/package-install.sh
+++ prototype/main/xfce-config/full/Main/package-install.sh
@@ -52,7 +52,7 @@ main_package_install () {
 
 	# sudo apt-get install $(cat package-list.txt)
 	# sudo apt-get install $(main_package_find_list_raw)
-	sudo apt-get install $(main_package_find_list)
+	# sudo apt-get install $(main_package_find_list)
 
 
 	##
@@ -74,6 +74,16 @@ main_package_install () {
 	# sudo dnf install $(cat package-list.txt)
 	# sudo dnf install $(main_package_find_list_raw)
 	# sudo dnf install $(main_package_find_list)
+
+
+	##
+	## ## Solus
+	##
+
+	# sudo eopkg install $(cat package-list.txt)
+	# sudo eopkg install $(main_package_find_list_raw)
+	sudo eopkg install $(main_package_find_list)
+
 
 }
 
